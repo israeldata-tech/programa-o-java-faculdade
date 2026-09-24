34c3cfeedb7c1552ec745a4680c004e72453238c
C:\Users\User\AppData\Local\Programs\Python\Python314\python.exe <israel281127@gmail.com>
2026-09-24 13:58:30 -0300

    logica de conversão de idade para dias

---
+++ exercicios em java/idade.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class idade {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("digite a sua idade em anos: ");
+        int i = s.nextInt();
+        System.out.println("digite sua idade em meses: ");
+        int m = s.nextInt();
+        System.out.println("digite a quantidade de dias: ");
+        int d = s.nextInt();
+        int dias = (i*365)+(m*30)+d;
+        System.out.println("sua idade em dias é: "+dias);
+    }
+}
