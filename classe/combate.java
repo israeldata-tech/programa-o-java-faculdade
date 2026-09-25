84e0858866822845b288741977fb0a834bab8bd3
C:\Users\User\AppData\Local\Programs\Python\Python314\python.exe <israel281127@gmail.com>
2026-09-25 17:19:39 -0300

    quebrando cabeça pra ver se descubro como fazer o loop funcionar

--- classe/combate.java
+++ classe/combate.java
@@ -1,8 +1,42 @@
 import java.util.Scanner;
 public class combate {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         personagem p1 = new personagem();
-
+        personagem p2 = new personagem();
+        System.out.println("digite o nome do primeiro personagem: ");
+        p1.nome=s.nextLine();
+        System.out.println("digite o nome da classe do primeiro personagem: ");
+        p1.classe=s.nextLine();
+        System.out.println("digite a vida maxima do primeiro personagem: ");
+        p1.vidam=s.nextInt();
+        p1.vida=p1.vidam;
+        System.out.println("digite quantos pontos de ataque o primeiro personagem tem: ");
+        p1.pa=s.nextDouble();
+        System.out.println("VOCE ENCONTROU UMA LOJA, QUANTAS POÇÕES COMPRA?");
+        p1.qtdp=s.nextInt();
+        System.out.println("QUAL A FORÇA DESSAS POÇÕES? ");
+        p1.forçap=s.nextDouble();
+        s.nextLine();
+        System.out.println();
+        System.out.println();
+        System.out.println("digite o nome do segundo personagem: ");
+        p2.nome=s.nextLine();
+        System.out.println("digite o nome da classe do segundo personagem: ");
+        p2.classe=s.nextLine();
+        System.out.println("digite a vida maxima do segundo personagem: ");
+        p2.vidam=s.nextInt();
+        p2.vida=p2.vidam;
+        System.out.println("digite quantos pontos de ataque o segundo personagem tem: ");
+        p2.pa=s.nextDouble();
+        System.out.println("VOCE ENCONTROU UMA LOJA, QUANTAS POÇÕES COMPRA?");
+        p2.qtdp=s.nextInt();
+        System.out.println("QUAL A FORÇA DESSAS POÇÕES? ");
+        p2.forçap=s.nextDouble();
+        System.out.println();
+        while(p1.vida>0 && p2.vida>0){
+            
+        }
     }
 }
 class personagem{
@@ -14,6 +48,7 @@ class personagem{
     int qtdp;
     double forçap;
     public void receberataque(double ataque){
+        
         vida-=ataque;
         if(vida<0){
             vida=0;
